01c1beb090908ca5bc1bd9bef974dd89f90f7622
imnnm <gvozdeffand@yandex.ru>
2020-03-14 19:51:55 +0300

    Changed layout for devise controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  layout :layout_by_resource
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:user) do |user_params|
       user_params.permit(:email, :password, :remember_me)
+    end
+  end
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
     end
   end
 end
